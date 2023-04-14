7dcc83bf78475f1eabc07c9de9deb138507474d8
Sergey Gorbut <gorbut_sa@magnit.ru>
2023-04-14 19:15:09 +0300

    added test to click on item from list

--- tests/AnotherPageTest.spec.js
+++ tests/AnotherPageTest.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Dropdown menu', async ({ browser }) => {
+test('Dropdown menu', async ({ browser }) => {
 
     const context = await browser.newContext();
     const page = await context.newPage();
---
+++ tests/SelectFromListTest.spec.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+
+test('Select item from list of items', async ({ page }) => {
+  
+    const searchInput = page.locator('[name="q"]');
+    const elementOfList = page.locator('.repo-list-item');
+    const programmingLanguage = "Java";
+    
+    await page.goto('https://github.com/');    // go to url
+    await searchInput.click;   // Click on search input
+    await searchInput.fill("Appium");  // Enter text to input
+    await searchInput.press('Enter');    // Press Enter on input
+    await page.waitForLoadState('networkidle')
+    // Click on repository with Java language
+    for(let i = 0; i < await elementOfList.count(); ++i)
+    {
+      if (await elementOfList.nth(i).locator("[itemprop='programmingLanguage']").textContent() == programmingLanguage)
+      {
+        await elementOfList.nth(i).locator('.v-align-middle').click();
+        break;
+      }
+    }
+    // await page.pause();
+
+  });
