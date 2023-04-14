f0fabe9a5f11b4feb75a9789fcdb98e9254e0954
Sergey Gorbut <sgorbut@gmail.com>
2023-04-14 09:16:36 +0300

    added test for open new page

---
+++ tests/AnotherPageTest.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+
+test.only('Dropdown menu', async ({ browser }) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.pause();
+    await page.goto('https://github.com/');    // go to url
+    console.log(await page.title());
+    await page.hover('.HeaderMenu-item:nth-child(1) > .HeaderMenu-link');   // Hover on "Product"
+    const [newPage] = await Promise.all([  // new page
+      context.waitForEvent('page'),
+      page.click('li:nth-child(5) > [href="https://github.blog"]')    // Click "Blog "
+    ])
+    const newPageHeader = await newPage.title();
+    console.log(newPageHeader);    // print page title
+    await newPage.close();  // close new page
+
+  });
